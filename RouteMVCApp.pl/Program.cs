1c0f1995207c336e90ccd4849a26b08d5bcfe31b
SI01029 <SaeedIbrahim01029@gmail.com>
2025-10-01 12:47:54 +0300

    Add project files.

---
+++ RouteMVCApp.pl/Program.cs
@@ -0,0 +1,43 @@
+namespace RouteMVCApp.pl
+{
+    public class Program
+    {
+        //Entry point
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            #region Configure Services [Add Service To DI Continer]
+
+            
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            #endregion
+
+            var app = builder.Build();
+
+            #region Configure K estral MiddleWares [Piplines
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            //verp: BaseUrl/Controller/action
+            app.MapControllerRoute(
+                name: "default", 
+            
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            #endregion
+            app.Run();
+        }
+    }
+}
---
+++ RouteMVCApp.pl/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RouteMVCApp.pl.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
